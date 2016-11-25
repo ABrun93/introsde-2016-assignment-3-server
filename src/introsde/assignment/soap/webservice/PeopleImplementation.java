3e943cdd27736832788580d4d311d8d85704aa06
ABrun93 <a.brun93@gmail.com>
2016-11-25 16:28:48 +0100

    Server

---
+++ src/introsde/assignment/soap/webservice/PeopleImplementation.java
@@ -0,0 +1,55 @@
+package introsde.assignment.soap.webservice;
+
+import introsde.assignment.soap.model.Person;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+//Service Implementation
+
+@WebService(endpointInterface = "introsde.assignment.soap.webservice.People",
+    serviceName="PeopleService")
+public class PeopleImplementation implements People {
+
+    @Override
+    public Person readPerson(int id) {
+        System.out.println("---> Reading Person by id = "+id);
+        Person p = Person.getPersonById(id);
+        if (p!=null) {
+            System.out.println("---> Found Person by id = "+id+" => "+p.getFirstname());
+        } else {
+            System.out.println("---> Didn't find any Person with  id = "+id);
+        }
+        return p;
+    }
+
+    @Override
+    public List<Person> getPeople() {
+        return Person.getAll();
+    }
+
+    @Override
+    public int addPerson(Person person) {
+        Person.savePerson(person);
+        return person.getIdPerson();
+    }
+
+    @Override
+    public int updatePerson(Person person) {
+        Person.updatePerson(person);
+        return person.getIdPerson();
+    }
+
+    @Override
+    public int deletePerson(int id) {
+        Person p = Person.getPersonById(id);
+        if (p!=null) {
+            Person.removePerson(p);
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
+
