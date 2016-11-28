b66f87eb44fb7ec92e2f693a98a31dac430dad53
ABrun93 <a.brun93@gmail.com>
2016-11-28 17:10:14 +0100

    Publisher for heroku

--- src/introsde/assignment/soap/endpoint/PeoplePublisher.java
+++ src/introsde/assignment/soap/endpoint/PeoplePublisher.java
@@ -2,21 +2,35 @@ package introsde.assignment.soap.endpoint;
 
 import introsde.assignment.soap.webservice.PeopleImplementation;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+
 import javax.xml.ws.Endpoint;
 
-public class PeoplePublisher {
-    public static String SERVER_URL = "http://localhost";
-    public static String PORT = "6903";
-    public static String BASE_URL = "/ws/people";
+public class PeoplePublisher 
+{
+    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
+    {
+        String PROTOCOL = "http://";
+        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+        String PORT = "6903";
+        String BASE_URL = "/ws/people";
+        
+        if (HOSTNAME.equals("127.0.0.1"))
+        {
+            HOSTNAME = "localhost";
+        }       
 
-    public static String getEndpointURL() {
-        return SERVER_URL+":"+PORT+BASE_URL;
-    }
-
-    public static void main(String[] args) {
-        String endpointUrl = getEndpointURL();
+        if (String.valueOf(System.getenv("PORT")) != "null")
+        {
+            PORT = String.valueOf(System.getenv("PORT"));
+        }
+        
+        String endpointUrl = PROTOCOL + HOSTNAME + ":" + PORT + BASE_URL;
+        
         System.out.println("Starting People Service...");
-        System.out.println("--> Published at = "+endpointUrl);
+        System.out.println("--> Published. Check out " + endpointUrl + "?wsdl");
         Endpoint.publish(endpointUrl, new PeopleImplementation());
     }
 }
